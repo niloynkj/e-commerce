935da06b8cd51101280891383229694d9783a43c
niloynkj <66770175+niloynkj@users.noreply.github.com>
2022-04-09 20:15:18 +0600

    Create index2.js

---
+++ index2.js
@@ -0,0 +1,108 @@
+var config = {
+    apiKey: "AIzaSyCWGbJi9qeD7h6BPhAKBoWupHv21Tge3ck",
+  authDomain: "effort-5efac.firebaseapp.com",
+  databaseURL: "https://effort-5efac.firebaseio.com",
+  projectId: "effort-5efac",
+  storageBucket: "effort-5efac.appspot.com",
+  messagingSenderId: "771931121287",
+  appId: "1:771931121287:web:9fe9abdcab7c369f277a22",
+  measurementId: "G-GLD9CY5VPF"};
+firebase.initializeApp(config);
+
+
+////////Profit////Arry////
+//////////////////////////
+
+
+(function() {
+  
+  var Future = angular.module('Future', [ "firebase"]);
+
+Future.controller('Maincontollers', ["$scope", "$firebaseArray", "$interval", function ($scope, $firebaseArray, $interval) {
+
+  $scope.sortType     = 'date'; // set the default sort type
+$scope.sortReverse  = false;  // set the default sort order
+$scope.searchFish   = 'Live';
+$scope.searchProfit   = 'Future';
+ $scope.showData = function( ){
+
+ $scope.itemsPerPage = 1000;
+ $scope.currentPage = 0;
+ 
+ var live = "BE-commerce/Post";
+var ref = firebase.database().ref().child(live);
+
+	$scope.pages = $firebaseArray(ref);
+     $scope.range = function() {
+    var rangeSize = 4;
+    var ps = [];
+    var begin;
+
+    begin = $scope.currentPage;
+    if ( begin > $scope.pageCount()-rangeSize ) {
+      begin = $scope.pageCount()-rangeSize+1;
+    }
+
+    for (var i=begin; i<begin+rangeSize; i++) {
+      ps.push(i);
+    }
+    return ps;
+  };
+
+  $scope.prevPage = function() {
+    if ($scope.currentPage > 0) {
+      $scope.currentPage--;
+    }
+  };
+
+  $scope.DisablePrevPage = function() {
+    return $scope.currentPage === 0 ? "disabled" : "";
+  };
+
+  $scope.pageCount = function() {
+    return Math.ceil($scope.pages.length/$scope.itemsPerPage)-1;
+  };
+
+  $scope.nextPage = function() {
+    if ($scope.currentPage < $scope.pageCount()) {
+      $scope.currentPage++;
+    }
+  };
+
+  $scope.DisableNextPage = function() {
+    return $scope.currentPage === $scope.pageCount() ? "disabled" : "";
+  };
+
+  $scope.setPage = function(n) {
+    $scope.currentPage = n;
+  };
+         
+};
+
+
+}]);
+
+angular.module('Future').filter('pagination', function(){
+  return function(input, begin) {
+    begin = parseInt(begin, 10);
+    return input.slice(begin);
+  };
+});
+
+  
+})();
+
+
+////////////Cost/////////Arry///
+////////////////////////////////
+
+
+
+ var firstDiv = document.getElementById('Future');
+
+    
+       // manually boostrapping the second app
+       angular.element(document).ready(function() {
+              angular.bootstrap(firstDiv, [ 'Future' ]);
+			  });
+	   
