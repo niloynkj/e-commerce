79464e9e5593deb8caa6fb941047ddcb95e0df91
niloynkj <66770175+niloynkj@users.noreply.github.com>
2022-04-09 21:04:13 +0600

    Update indexF.js

--- indexF.js
+++ indexF.js
@@ -14,86 +14,7 @@ firebase.initializeApp(config);
 //////////////////////////
 
 
-(function() {
-  
-  var FProduct = angular.module('FProduct', [ "firebase"]);
-
-FProduct.controller('Maincontollersf', ["$scope", "$firebaseArray", "$interval", function ($scope, $firebaseArray, $interval) {
-
-  $scope.sortType     = 'date'; // set the default sort type
-$scope.sortReverse  = false;  // set the default sort order
-$scope.searchFish   = 'Live';
-$scope.searchProfit   = 'Future';
- $scope.showData = function( ){
-
- $scope.itemsPerPage = 1000;
- $scope.currentPage = 0;
- 
- var live = "BE-commerce/Post";
-var ref = firebase.database().ref().child(live);
-
-	$scope.pages = $firebaseArray(ref);
-     $scope.range = function() {
-    var rangeSize = 4;
-    var ps = [];
-    var begin;
-
-    begin = $scope.currentPage;
-    if ( begin > $scope.pageCount()-rangeSize ) {
-      begin = $scope.pageCount()-rangeSize+1;
-    }
-
-    for (var i=begin; i<begin+rangeSize; i++) {
-      ps.push(i);
-    }
-    return ps;
-  };
-
-  $scope.prevPage = function() {
-    if ($scope.currentPage > 0) {
-      $scope.currentPage--;
-    }
-  };
-
-  $scope.DisablePrevPage = function() {
-    return $scope.currentPage === 0 ? "disabled" : "";
-  };
-
-  $scope.pageCount = function() {
-    return Math.ceil($scope.pages.length/$scope.itemsPerPage)-1;
-  };
-
-  $scope.nextPage = function() {
-    if ($scope.currentPage < $scope.pageCount()) {
-      $scope.currentPage++;
-    }
-  };
-
-  $scope.DisableNextPage = function() {
-    return $scope.currentPage === $scope.pageCount() ? "disabled" : "";
-  };
-
-  $scope.setPage = function(n) {
-    $scope.currentPage = n;
-  };
-         
-};
-
-
-}]);
-
-angular.module('FProduct').filter('pagination', function(){
-  return function(input, begin) {
-    begin = parseInt(begin, 10);
-    return input.slice(begin);
-  };
-});
-
-  
-})();
-
-
-////////////Cost/////////Arry///
+////Cost/////////Arry///
 ////////////////////////////////
 
 
@@ -173,12 +94,93 @@ angular.module('Letest').filter('pagination', function(){
 });
 })();
 
+////////////////////OO/////////////
+
+(function() {
+  
+  var FProduct = angular.module('FProduct', [ "firebase"]);
+
+FProduct.controller('Maincontollersf', ["$scope", "$firebaseArray", "$interval", function ($scope, $firebaseArray, $interval) {
+
+  $scope.sortType     = 'date'; // set the default sort type
+$scope.sortReverse  = false;  // set the default sort order
+$scope.searchFish   = 'Live';
+$scope.searchProfit   = 'Future';
+ $scope.showData = function( ){
+
+ $scope.itemsPerPage = 1000;
+ $scope.currentPage = 0;
+ 
+ var live = "BE-commerce/Post";
+var ref = firebase.database().ref().child(live);
+
+	$scope.pages = $firebaseArray(ref);
+     $scope.range = function() {
+    var rangeSize = 4;
+    var ps = [];
+    var begin;
+
+    begin = $scope.currentPage;
+    if ( begin > $scope.pageCount()-rangeSize ) {
+      begin = $scope.pageCount()-rangeSize+1;
+    }
+
+    for (var i=begin; i<begin+rangeSize; i++) {
+      ps.push(i);
+    }
+    return ps;
+  };
+
+  $scope.prevPage = function() {
+    if ($scope.currentPage > 0) {
+      $scope.currentPage--;
+    }
+  };
+
+  $scope.DisablePrevPage = function() {
+    return $scope.currentPage === 0 ? "disabled" : "";
+  };
+
+  $scope.pageCount = function() {
+    return Math.ceil($scope.pages.length/$scope.itemsPerPage)-1;
+  };
+
+  $scope.nextPage = function() {
+    if ($scope.currentPage < $scope.pageCount()) {
+      $scope.currentPage++;
+    }
+  };
+
+  $scope.DisableNextPage = function() {
+    return $scope.currentPage === $scope.pageCount() ? "disabled" : "";
+  };
+
+  $scope.setPage = function(n) {
+    $scope.currentPage = n;
+  };
+         
+};
+
+
+}]);
+
+angular.module('FProduct').filter('pagination', function(){
+  return function(input, begin) {
+    begin = parseInt(begin, 10);
+    return input.slice(begin);
+  };
+});
+
+  
+})();
+
+
+//////////
 
 
 
- var firstDiv = document.getElementById('FProduct');
  var SecondDiv = document.getElementById('Letest');
-
+ var firstDiv = document.getElementById('FProduct');
     
        // manually boostrapping the second app
        angular.element(document).ready(function() {
